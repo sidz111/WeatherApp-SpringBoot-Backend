718525a904ae7e97a1fc68003d3970ead1d9448a
Shuddhodhan Surwade <119784108+sidz111@users.noreply.github.com>
2024-07-12 03:32:29 +0530

    Add CrossOrigin in controller class

--- src/main/java/com/example/controller/WeatherController.java
+++ src/main/java/com/example/controller/WeatherController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import com.example.model.WeatherResponse;
 import com.example.service.WeatherService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class WeatherController {
 
     @Autowired
